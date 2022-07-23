2264e7578d52edd4877c002f4592c836c39526de
Chukwuemeka <okocha007@gmail.com>
2022-07-23 17:05:09 +0100

    chatbox is responding well

--- calScript.js
+++ calScript.js
@@ -1,15 +1,27 @@
-let msg= document.getElementById("txtInput");
-let btn = document.getElementById("button");
-let list = document.getElementById("list");
+let msg= document.getElementById("custInput");
+let msg2= document.getElementById("suppInput");
+let cust = document.getElementById("button");
+let supp = document.getElementById("button2");
+let list = document.getElementById("customer");
+let list2 = document.getElementById("support");
 
-btn.addEventListener("click", function(){
-    if(msg =""){
-        prompt("type a message to be sent");
-    }
-    else{
-        let createmsg= document.createElement("li");
-        createmsg.innerHTML=msg.value;
+cust.addEventListener("click", function(){
+   // if(msg.value ==""){
+     //   alert("please type a message and click send");
+   // }
+    let createmsg= document.createElement("li");
+        createmsg.innerHTML= msg.value;
+        list2.appendChild(createmsg);
+        msg.value="";
+
+});
+supp.addEventListener("click", function(){
+//    if(msg2.value ==""){
+  //      alert("please type a message and click send");
+    //}
+    let createmsg= document.createElement("li");
+        createmsg.innerHTML= msg2.value;
         list.appendChild(createmsg);
-        msg="";
-    }
+        msg2.value="";
+
 });
